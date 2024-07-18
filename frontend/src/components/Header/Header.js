0315829899eec8a966a09f5b59f43375c20eba11
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-07-18 20:37:16 +0300

    header style added

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,15 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import classes from "./header.module.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './header.module.css';
 
 export default function Header() {
   const user = {
-    name: "John",
-  };
+    name:'John',
+  }
 
-  const cart = {
-    totalCount: 10,
-  };
+  const cart={
+    totalCount:10,
+  } 
+
+  const logout =()=>{}
 
   return (
     <header className={classes.header}>
@@ -17,6 +19,31 @@ export default function Header() {
         <Link to="/" className={classes.logo}>
           Food Mine!
         </Link>
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/dashboard">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                </div>
+              </li>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+
+            <li>
+              <Link to="/cart">
+                Cart
+                {cart.totalCount > 0 && (
+                  <span className={classes.cart_count}>{cart.totalCount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
